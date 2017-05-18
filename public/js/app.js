66afc00a01362aaf3da90924ef521edd0eae1e73
Cathleen Wright <cathleenmwright@gmail.com>
2017-05-18 16:23:37 -0400

    Boilerplate setup

---
+++ public/js/app.js
@@ -0,0 +1,9 @@
+// console.log('Excuses app is connected.');
+
+var app = angular.module('excuses-app', []);
+
+app.controller('mainController', ['$http', function($http){
+
+  this.title = "Excuses, Excuses!"
+
+}]); // mainController END
