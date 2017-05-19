27bc2cea6b938ac9ad12dfa4fbab284cdbeb7f6c
j-hha <j-hha@bla.bla>
2017-05-19 12:06:04 -0400

    preparing for first deploy

--- public/js/app.js
+++ public/js/app.js
@@ -3,21 +3,23 @@
 var app = angular.module('excuses-app', []);
 
 app.controller('mainController', ['$http', '$scope', function($http, $scope){
-
   this.title = "Excuses, Excuses!"
+  
+  // this is the url for our backend, inject scope to your controllers and
+  // you should be able to use this variable anywhere
+  $scope.baseUrl = 'https://perfect-excuse.herokuapp.com/'
 
+  // this is a function that checks if a user is logged in, inject scope to your
+  // controllers and you should be able to use this variable anywhere
   $scope.userIsLoggedIn = function() {
-    if(localStorage.getItem('token')) {
+    token = localStorage.getItem('token');
+    if(token !== 'undefined' && token !== undefined && token !== null) {
       console.log('The user is logged in.');
       return true;
     } else {
       console.log('The user is NOT logged in.');
       return false;
     }
-  };
-
-  $scope.test = function() {
-    console.log('THIS WORKS!');
   };
 
 }]); // mainController END
