5ddd868a228f340e20f5f2c0f0198e2fad95f059
Cathleen Wright <cathleenmwright@gmail.com>
2017-05-19 12:21:41 -0400

    Delete with Removal

--- public/js/controllers/excuses-controller.js
+++ public/js/controllers/excuses-controller.js
@@ -3,13 +3,27 @@ function($http, $scope) {
 
   this.excuses = [];
 
+  this.getExcuses = function() {
     $http({
-    method: 'GET',
-    url: 'http://localhost:3000/relations',
-  }).then(function(response){
-    console.log('all relations', response);
-    this.excuses = response.data;
-  }.bind(this));
+      method: 'GET',
+      url: 'http://localhost:3000/relations',
+    }).then(function(response){
+      console.log('all relations', response);
+      this.excuses = response.data;
+    }.bind(this));
+  };
 
+  this.deleteExcuse = function(id){
+    $http({
+     method: 'DELETE',
+     url: 'http://localhost:3000/relations/'+ id,
+    }).then(function(response){
+     console.log("Deleted: ", response);
+     this.getExcuses();
+    }.bind(this));
+
+  };
+
+  this.getExcuses();
 
 }]);
