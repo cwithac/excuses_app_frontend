0c2832e4c5e02d5af5f713cc92d07e42d0a235da
j-hha <j-hha@bla.bla>
2017-05-22 13:47:41 -0400

    edit options show up on 'show my excuses' click

--- public/js/app.js
+++ public/js/app.js
@@ -41,7 +41,7 @@ app.controller('mainController', ['$http', '$scope', function($http, $scope){
 
   // this is the url for our backend, inject scope to your controllers and
   // you should be able to use this variable anywhere
-  $scope.baseUrl = 'https://perfect-excuse.herokuapp.com/' /*'http://localhost:3000/'*/
+  $scope.baseUrl = 'https://perfect-excuse.herokuapp.com/' /*'http://localhost:3000/'*/ 
 
   // scope variable holding userData, available to all controllers
   $scope.userData = {};
@@ -85,7 +85,6 @@ app.controller('mainController', ['$http', '$scope', function($http, $scope){
       console.log('The user is logged in.');
       $scope.userData.username = JSON.parse(localStorage.getItem('username'));
       $scope.userData.id = JSON.parse(localStorage.getItem('user_id'));
-      $scope.showButtons();
       return true;
     } else {
       $scope.userData = {};
